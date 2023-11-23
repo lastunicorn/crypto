adc0eeb338f0ada8672ca714a9afdc9cf613eb4b
Alexandru Iuga <alex.iuga@gmail.com>
2023-11-23 14:22:49 +0200

    refactored the cert parent verification.

--- sources/Crypto.Application/Steps/VerifyCertificateParentStep.cs
+++ sources/Crypto.Application/Steps/VerifyCertificateParentStep.cs
@@ -24,24 +24,34 @@ public class VerifyCertificateParentStep : StepBase
 
     private bool IsCertificateFromCurrentParent()
     {
-        X509SubjectKeyIdentifierExtension subjectKeyIdentifier = ParentCertificate.Extensions.OfType<X509Extension>()
-            .Where(x => x.Oid.FriendlyName == "Subject Key Identifier")
-            .FirstOrDefault() as X509SubjectKeyIdentifierExtension;
-
-        X509Extension aki = Certificate.Extensions
-            .OfType<X509Extension>()
-            .FirstOrDefault(x => x.Oid.Value == OidValues.AuthorityKeyIdentifier);
+        X509SubjectKeyIdentifierExtension subjectKeyIdentifier = GetSki();
+        X509Extension aki = GetAki();
 
         if (aki == null)
-        {
             return false;
-        }
 
-        string parentSki = subjectKeyIdentifier?.Format(false) ?? "";
-        string certAki = aki.Format(false) ?? "";
-        if (certAki.Contains(parentSki))
+        string parentSki = subjectKeyIdentifier?.Format(false) ?? string.Empty;
+        string certAki = aki.Format(false) ?? string.Empty;
+
+        bool areCertificatesRelated = certAki.Contains(parentSki);
+        
+        if (areCertificatesRelated)
             Console.WriteLine("Certificate is from current parent");
 
-        return true;
+        return areCertificatesRelated;
+    }
+
+    private X509SubjectKeyIdentifierExtension GetSki()
+    {
+        X509Extension x509Extension = ParentCertificate.Extensions
+            .FirstOrDefault(x => x.Oid?.FriendlyName == "Subject Key Identifier");
+
+        return x509Extension as X509SubjectKeyIdentifierExtension;
+    }
+
+    private X509Extension GetAki()
+    {
+        return Certificate.Extensions
+            .FirstOrDefault(x => x.Oid?.Value == OidValues.AuthorityKeyIdentifier);
     }
 }
